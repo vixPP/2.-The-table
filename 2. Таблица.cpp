c07b9bdfd95ebe197ecca03c3583351144322c2b
vix <churink9@gmail.com>
2024-06-23 19:29:29 +0300

    first commit

---
+++ 2. Таблица.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+template <typename T>
+class Table 
+{
+private:
+    std::vector<std::vector<T>> data;
+    int rows;
+    int cols;
+
+public:
+    Table(int rows, int cols) : rows(rows), cols(cols) 
+    {
+        data.resize(rows, std::vector<T>(cols));
+    }
+
+    const std::vector<T>& operator[](int index) const 
+    {
+        return data[index];
+    }
+
+    std::vector<T>& operator[](int index) 
+    {
+        return data[index];
+    }
+
+    int Size() const 
+    {
+        return rows * cols;
+    }
+};
+
+int main() 
+{
+    Table<int> test(2, 3);
+    test[0][0] = 4;
+    std::cout << test[0][0]; 
+    return 0;
+}
